5ffc85a69d436b42e02cab52673d5b4353943341
Thomas Figg <tef@pharmacon.(none)>
2009-03-23 01:10:12 +0000

    refactoring

--- rev.py
+++ rev.py
@@ -1,24 +1,11 @@
-"""Disassembler of Python byte code into mnemonics."""
-
 import sys
 import types
 
 from opcode import *
-from opcode import __all__ as _opcodes_all
 
-__all__ = ["dis", "disassemble", "distb", "disco",
-           "findlinestarts", "findlabels"] + _opcodes_all
-del _opcodes_all
-
-def dis(x=None):
-    """Disassemble classes, methods, functions, or code.
-
-    With no argument, disassemble the last traceback.
-
-    """
+def rev(x=None):
     if x is None:
-        distb()
-        return
+        x = get_trackback()
     if type(x) is types.InstanceType:
         x = x.__class__
     if hasattr(x, 'im_func'):
@@ -26,6 +13,26 @@ def dis(x=None):
     if hasattr(x, 'func_code'):
         x = x.func_code
     if hasattr(x, '__dict__'):
+        return reverse_class(x)
+    elif hasattr(x, 'co_code'):
+        return reverse(x)
+    elif isinstance(x, str):
+        return reverse_string(x)
+    else:
+        raise TypeError, \
+              "don't know how to reverse%s objects" % \
+              type(x).__name__
+
+def get_trackback(tb=None):
+    if tb is None:
+        try:
+            tb = sys.last_traceback
+        except AttributeError:
+            raise RuntimeError, "no last traceback to disassemble"
+        while tb.tb_next: tb = tb.tb_next
+    reverse(tb.tb_frame.f_code, tb.tb_lasti)
+
+def reverse_class(x):
         items = x.__dict__.items()
         items.sort()
         for name, x1 in items:
@@ -35,79 +42,72 @@ def dis(x=None):
                             types.ClassType):
                 print "Disassembly of %s:" % name
                 try:
-                    dis(x1)
+                    reverse(x1)
                 except TypeError, msg:
                     print "Sorry:", msg
                 print
-    elif hasattr(x, 'co_code'):
-        disassemble(x)
-    elif isinstance(x, str):
-        disassemble_string(x)
-    else:
-        raise TypeError, \
-              "don't know how to disassemble %s objects" % \
-              type(x).__name__
 
-def distb(tb=None):
-    """Disassemble a traceback (default: last traceback)."""
-    if tb is None:
-        try:
-            tb = sys.last_traceback
-        except AttributeError:
-            raise RuntimeError, "no last traceback to disassemble"
-        while tb.tb_next: tb = tb.tb_next
-    disassemble(tb.tb_frame.f_code, tb.tb_lasti)
+def reverse(co, lasti=-1):
+    r = Reverser(co, lasti)
+    return r.rev()
 
-def disassemble(co, lasti=-1):
-    """Disassemble a code object."""
-    code = co.co_code
-    labels = findlabels(code)
-    linestarts = dict(findlinestarts(co))
-    n = len(code)
-    i = 0
-    extended_arg = 0
-    free = None
-    while i < n:
-        c = code[i]
-        op = ord(c)
-        if i in linestarts:
-            if i > 0:
-                print
-            print "%3d" % linestarts[i],
-        else:
-            print '   ',
 
-        if i == lasti: print '-->',
-        else: print '   ',
-        if i in labels: print '>>',
-        else: print '  ',
-        print repr(i).rjust(4),
-        print opname[op].ljust(20),
-        i = i+1
-        if op >= HAVE_ARGUMENT:
-            oparg = ord(code[i]) + ord(code[i+1])*256 + extended_arg
-            extended_arg = 0
-            i = i+2
-            if op == EXTENDED_ARG:
-                extended_arg = oparg*65536L
-            print repr(oparg).rjust(5),
-            if op in hasconst:
-                print '(' + repr(co.co_consts[oparg]) + ')',
-            elif op in hasname:
-                print '(' + co.co_names[oparg] + ')',
-            elif op in hasjrel:
-                print '(to ' + repr(i + oparg) + ')',
-            elif op in haslocal:
-                print '(' + co.co_varnames[oparg] + ')',
-            elif op in hascompare:
-                print '(' + cmp_op[oparg] + ')',
-            elif op in hasfree:
-                if free is None:
-                    free = co.co_cellvars + co.co_freevars
-                print '(' + free[oparg] + ')',
-        print
 
-def disassemble_string(code, lasti=-1, varnames=None, names=None,
+class Reverser(object):
+
+    def __init__(self, co, lasti=-1):
+        self.code = co.co_code
+        self.labels = findlabels(self.code)
+        self.linestarts = dict(findlinestarts(co))
+        self.n = len(self.code)
+        self.lasti = lasti
+        self.co = co
+
+    def rev(self):
+        i = 0
+        extended_arg = 0
+        free = None
+        while i < self.n:
+            c = self.code[i]
+            op = ord(c)
+            if i in self.linestarts:
+                if i > 0:
+                    print
+                print "%3d" % self.linestarts[i],
+            else:
+                print '   ',
+
+            if i == self.lasti: print '-->',
+            else: print '   ',
+            if i in self.labels: print '>>',
+            else: print '  ',
+            print repr(i).rjust(4),
+            print opname[op].ljust(20),
+            i = i+1
+            if op >= HAVE_ARGUMENT:
+                oparg = ord(self.code[i]) + ord(self.code[i+1])*256 + extended_arg
+                extended_arg = 0
+                i = i+2
+                if op == EXTENDED_ARG:
+                    extended_arg = oparg*65536L
+                print repr(oparg).rjust(5),
+                if op in hasconst:
+                    print '(' + repr(self.co.co_consts[oparg]) + ')',
+                elif op in hasname:
+                    print '(' + self.co.co_names[oparg] + ')',
+                elif op in hasjrel:
+                    print '(to ' + repr(i + oparg) + ')',
+                elif op in haslocal:
+                    print '(' + self.co.co_varnames[oparg] + ')',
+                elif op in hascompare:
+                    print '(' + cmp_op[oparg] + ')',
+                elif op in hasfree:
+                    if free is None:
+                        free = co.co_cellvars + co.co_freevars
+                    print '(' + free[oparg] + ')',
+            print
+
+def reversee_string(code, lasti=-1, varnames=None, names=None,
                        constants=None):
     labels = findlabels(code)
     n = len(code)
@@ -147,7 +147,6 @@ def disassemble_string(code, lasti=-1, varnames=None, names=None,
                 print '(' + cmp_op[oparg] + ')',
         print
 
-disco = disassemble                     # XXX For backwards compatibility
 
 def findlabels(code):
     """Detect all offsets in a byte code which are jump targets.
@@ -218,7 +217,7 @@ def _test():
     else:
         fn = "<stdin>"
     code = compile(source, fn, "exec")
-    dis(code)
+    print rev(code)
 
 if __name__ == "__main__":
     _test()
